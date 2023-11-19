9d37f411a43467db62a81da213b972a5e3c394c7
Daniel Asena <asenadaniel@gmail.com>
2023-11-19 23:50:11 +0100

    up

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -88,20 +88,20 @@ function CartPage() {
     });
   };
   return (
-    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-900 lg:flex-row">
+    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-900 lg:flex-row mb-3" >
       <div
-        className="h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:1/2 lg:px-20 xl:px-40" /*Product Container */
+        className=" p-4 flex flex-col justify-center      overflow-scroll   lg:h-full lg:w-2/3 2xl:1/2 lg:px-20 xl:px-40 " /*Product Container */
       >
         {products.map(item => (
 
           <div
-            className="flex items-center justify-between mb-5" /*Single Item */ key={item.id}>
+            className="flex items-center  justify-between   mb-5" /*Single Item */ key={item.id}>
             <Image src={item.img} alt="pic" width={100} height={100} />
-            <div className="">
+            <div className=" text-center">
               <h1 className="uppercase text-xl font-bold">{item.title}</h1>
               <span>Large</span>
             </div>
-            <h2 className="font-bold">{item.price}</h2>
+            <h2 className="font-bold">£{item.price}</h2>
             <span className="cursor-pointer" onClick={() => {
               dispatch(removeItem(item.id));
               notifyRemove();
